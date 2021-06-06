d76742a97f91d690892e077d68a9213903baccf2
Alex Roper <alex@aroper.net>
2021-06-06 11:19:51 -0700

    Handle release build and other scenarios without a git repo gracefully.

--- src/compile_time.rs
+++ src/compile_time.rs
@@ -8,11 +8,11 @@ use std::time::SystemTime;
 pub fn write_stamp_file_at_compile_time(repo_path: &Path, out_dir: &Path) {
     let dest_path = Path::new(&out_dir).join("generated_stamp.rs");
 
-    let rev = git_revision(&repo_path);
-    let clean = git_clean(&repo_path);
+    let rev = git_revision(&repo_path).unwrap_or("<no git rev>".into());
+    let clean = git_clean(&repo_path).unwrap_or(true);
 
     let git_revision_cleanness = git_revision_cleanness(&rev, clean);
-    let git_status = git_status(&repo_path);
+    let git_status = git_status(&repo_path).unwrap_or("<no git status>".into());
 
     let mut fd = std::fs::File::create(&dest_path).unwrap();
 
@@ -46,42 +46,45 @@ pub const BUILD_STAMP: build_stamp::BuildStamp = build_stamp::BuildStamp{{
     .unwrap();
 }
 
-fn git_status(repo: &Path) -> String {
+fn git_status(repo: &Path) -> Option<String> {
     let output = Command::new("git")
         .current_dir(&repo)
         .arg("status")
         .output()
-        .expect("git status failed");
-    std::str::from_utf8(&output.stdout)
-        .expect("git status output not utf-8")
-        .to_string()
+        .ok()?;
+    std::str::from_utf8(&output.stdout).map(Into::into).ok()
 }
 
-fn git_clean(repo: &Path) -> bool {
+fn git_clean(repo: &Path) -> Option<bool> {
     let git_status_porcelain = Command::new("git")
         .current_dir(&repo)
         .arg("status")
         .arg("--porcelain")
         .output()
-        .unwrap()
+        .ok()?
         .stdout;
 
     // FIXME: This is probably fragile.
     std::str::from_utf8(&git_status_porcelain)
-        .unwrap()
-        .trim()
-        .is_empty()
+        .map(str::trim)
+        .map(str::is_empty)
+        .ok()
 }
 
-fn git_revision(repo: &Path) -> String {
+fn git_revision(repo: &Path) -> Option<String> {
     let output = Command::new("git")
         .current_dir(&repo)
         .arg("show-ref")
         .arg("--head")
         .arg("HEAD")
         .output()
-        .unwrap();
-    std::str::from_utf8(&output.stdout).unwrap()[..40].to_string()
+        .ok()?
+        .stdout;
+    if output.len() < 40 {
+        None
+    } else {
+        Some(std::str::from_utf8(&output).ok()?[..40].to_string())
+    }
 }
 
 fn git_revision_cleanness(rev: &str, clean: bool) -> String {
